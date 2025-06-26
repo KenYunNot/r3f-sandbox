c90e7cf7cddbc56df0ae8f1f96bff6c34b7c9365
Ken Yun <yunhaen@gmail.com>
2025-06-26 14:03:34 -0400

    feat: gsap for smooth animations in scene

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import type { Color, Mesh } from 'three';
 import { Canvas } from '@react-three/fiber';
-import './App.css';
 import { OrbitControls } from '@react-three/drei';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import './App.css';
 
 function App() {
+  const canvas = React.useRef<HTMLDivElement>(null);
+  const timeline = gsap.timeline({ defaults: { duration: 1 } });
+
+  useGSAP(() => {
+    timeline.fromTo('nav', { y: '-100%' }, { y: '0%' });
+  });
+
   return (
     <>
       <nav>
@@ -14,15 +24,18 @@ function App() {
         </ul>
       </nav>
       <h1 className='title'>Give it a spin</h1>
-      <div id='canvas-container'>
+      <div
+        ref={canvas}
+        id='canvas-container'
+      >
         <Canvas
           camera={{ fov: 45, position: [0, 0, 10] }}
           dpr={2}
         >
-          <mesh>
-            <sphereGeometry />
-            <meshStandardMaterial color={'#00ff83'} />
-          </mesh>
+          <Sphere
+            color='#00ff83'
+            timeline={timeline}
+          />
           <pointLight
             intensity={15}
             decay={1}
@@ -40,5 +53,26 @@ function App() {
     </>
   );
 }
+
+const Sphere = ({
+  color = '0xffffff',
+  timeline,
+}: {
+  color: string | number | Color;
+  timeline: ReturnType<typeof gsap.timeline>;
+}) => {
+  const sphereMesh = React.useRef<Mesh>(null!);
+
+  useGSAP(() => {
+    timeline.fromTo(sphereMesh.current.scale, { x: 0, y: 0, z: 0 }, { x: 1, y: 1, z: 1 });
+  });
+
+  return (
+    <mesh ref={sphereMesh}>
+      <sphereGeometry />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
 
 export default App;
