98f2b79f0062e526c8ade2400a28e2e36d844641
Ken Yun <yunhaen@gmail.com>
2025-06-26 11:42:53 -0400

    feat: basic layout for landing page

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,39 @@ import { OrbitControls } from '@react-three/drei';
 
 function App() {
   return (
-    <div id='canvas-container'>
-      <Canvas
-        camera={{ fov: 45, position: [0, 0, 10] }}
-        dpr={2}
-      >
-        <mesh>
-          <sphereGeometry />
-          <meshStandardMaterial color={'#00ff83'} />
-        </mesh>
-        <pointLight
-          intensity={15}
-          decay={1}
-          position={[0, 10, 10]}
-        />
-        <OrbitControls
-          enableDamping={true}
-          enablePan={false}
-          enableZoom={false}
-          autoRotate={true}
-          autoRotateSpeed={10}
-        />
-      </Canvas>
-    </div>
+    <>
+      <nav>
+        <a href='/'>Sphere</a>
+        <ul>
+          <li>Explore</li>
+          <li>Create</li>
+        </ul>
+      </nav>
+      <h1 className='title'>Give it a spin</h1>
+      <div id='canvas-container'>
+        <Canvas
+          camera={{ fov: 45, position: [0, 0, 10] }}
+          dpr={2}
+        >
+          <mesh>
+            <sphereGeometry />
+            <meshStandardMaterial color={'#00ff83'} />
+          </mesh>
+          <pointLight
+            intensity={15}
+            decay={1}
+            position={[0, 10, 10]}
+          />
+          <OrbitControls
+            enableDamping={true}
+            enablePan={false}
+            enableZoom={false}
+            autoRotate={true}
+            autoRotateSpeed={10}
+          />
+        </Canvas>
+      </div>
+    </>
   );
 }
 
