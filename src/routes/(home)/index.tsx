86309f915101c83c742ad55888fe026c06d354d4
Ken Yun <yunhaen@gmail.com>
2025-06-28 10:39:38 -0400

    fix: create grid item component

--- src/routes/(home)/index.tsx
+++ src/routes/(home)/index.tsx
@@ -9,49 +9,28 @@ const Home = () => {
 
   useGSAP(
     () => {
-      const gridItems = gsap.utils.toArray('.grid-item') as HTMLElement[];
-      gridItems.forEach((gridItem) => {
-        const hoverAnimation = gsap
-          .timeline({ paused: true, defaults: { duration: 0.5, ease: 'power1.out' } })
-          .fromTo(gridItem.querySelector('.hover-banner'), { opacity: 0 }, { opacity: 1 });
-
-        gridItem.addEventListener('mouseenter', () => hoverAnimation.play());
-        gridItem.addEventListener('mousemove', (event) => {
-          const bounds = gridItem.getBoundingClientRect();
-          const offsetX = event.clientX - bounds.left;
-          const offsetY = event.clientY - bounds.top;
-          gsap.to(gridItem.querySelector('.hover-banner'), {
-            background: `radial-gradient(circle at ${offsetX}px ${offsetY}px, green, blue)`,
-            duration: 1,
-            ease: 'power1.out',
-          });
-        });
-        gridItem.addEventListener('mouseleave', () => hoverAnimation.reverse());
-      });
-
       gsap
         .timeline()
         .from('.heading', { opacity: 0, y: '25%', delay: 0.6, ease: 'power1.out' })
-        .from('.grid-item', { opacity: 0, y: '25%', delay: 0.3, stagger: 0.3, ease: 'elastic.out' });
+        .from('.grid-item', {
+          opacity: 0,
+          y: '25%',
+          delay: 0.3,
+          stagger: 0.3,
+          ease: 'elastic.out',
+        });
     },
     { scope: container }
   );
 
   return (
-    <div
-      ref={container}
-      className='container'
-    >
+    <div ref={container} className='container'>
       <div className='heading'>
         <h1>R3F and GSAP Sandbox</h1>
         <p>Check out what I learned so far!</p>
       </div>
       <div className='grid-container'>
-        <div className='grid-item'>
-          <div className='hover-banner' />
-          <h2>
-            <a href='developedbyed-followalong'>Three.js Crash Course For Beginners</a>
-          </h2>
+        <GridItem href='developedbyed-followalong' title='Three.js Crash Course for Beginners'>
           <p>
             I did a follow-along with this{' '}
             <a
@@ -61,16 +40,55 @@ const Home = () => {
               video
             </a>{' '}
             by{' '}
-            <a
-              href='https://www.youtube.com/@developedbyed'
-              target='_blank'
-            >
+            <a href='https://www.youtube.com/@developedbyed' target='_blank'>
               developedbyed
             </a>{' '}
-            but I implemented it in React Three Fiber. The tutorial is originally done in vanilla Three.js.
+            but I implemented it in React Three Fiber. The tutorial is originally done in vanilla
+            Three.js.
           </p>
-        </div>
+        </GridItem>
       </div>
+    </div>
+  );
+};
+
+const GridItem = ({
+  href,
+  title,
+  children,
+}: {
+  href: string;
+  title: string;
+  children: React.ReactNode;
+}) => {
+  const gridItemRef = React.useRef<HTMLDivElement>(null!);
+
+  useGSAP(() => {
+    const hoverAnimation = gsap
+      .timeline({ paused: true, defaults: { duration: 0.5, ease: 'power1.out' } })
+      .fromTo(gridItemRef.current.querySelector('.hover-banner'), { opacity: 0 }, { opacity: 1 });
+
+    gridItemRef.current.addEventListener('mouseenter', () => hoverAnimation.play());
+    gridItemRef.current.addEventListener('mousemove', (event) => {
+      const bounds = gridItemRef.current.getBoundingClientRect();
+      const offsetX = event.clientX - bounds.left;
+      const offsetY = event.clientY - bounds.top;
+      gsap.to(gridItemRef.current.querySelector('.hover-banner'), {
+        background: `radial-gradient(circle at ${offsetX}px ${offsetY}px, green, blue)`,
+        duration: 1,
+        ease: 'power1.out',
+      });
+    });
+    gridItemRef.current.addEventListener('mouseleave', () => hoverAnimation.reverse());
+  });
+
+  return (
+    <div ref={gridItemRef} className='grid-item'>
+      <div className='hover-banner' />
+      <h2>
+        <a href={href}>{title}</a>
+      </h2>
+      <p>{children}</p>
     </div>
   );
 };
