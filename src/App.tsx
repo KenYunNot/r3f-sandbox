3283379fb74ccdb8214a9fce395affe72353235c
Ken Yun <yunhaen@gmail.com>
2025-06-28 10:45:35 -0400

    feat: add mouse-follow route

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,17 @@ import Home from './routes/(home)/index.tsx';
 import DevelopedbyedFollowalong from './routes/developedbyedFollowalong/index.tsx';
 
 import './App.css';
+import MouseFollow from './routes/mouseFollow/index.tsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          index
-          element={<Home />}
-        />
+        <Route index element={<Home />} />
 
-        <Route
-          path='developedbyed-followalong'
-          element={<DevelopedbyedFollowalong />}
-        />
+        <Route path='developedbyed-followalong' element={<DevelopedbyedFollowalong />} />
+
+        <Route path='mouse-follow' element={<MouseFollow />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/mouseFollow/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MouseFollow = () => {
+  return <div>MouseFollow</div>;
+};
+
+export default MouseFollow;
