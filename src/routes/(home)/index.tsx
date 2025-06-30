1290a2414800fa21ddb1a8e002638797cea7f556
Ken Yun <yunhaen@gmail.com>
2025-06-30 16:06:36 -0400

    feat: make 3d object follow mouse

--- src/routes/(home)/index.tsx
+++ src/routes/(home)/index.tsx
@@ -9,28 +9,31 @@ const Home = () => {
 
   useGSAP(
     () => {
-      gsap
-        .timeline()
-        .from('.heading', { opacity: 0, y: '25%', delay: 0.6, ease: 'power1.out' })
-        .from('.grid-item', {
-          opacity: 0,
-          y: '25%',
-          delay: 0.3,
-          stagger: 0.3,
-          ease: 'elastic.out',
-        });
+      gsap.timeline().from('.heading', { opacity: 0, y: '25%', delay: 0.6, ease: 'power1.out' }).from('.grid-item', {
+        opacity: 0,
+        y: '25%',
+        delay: 0.3,
+        stagger: 0.3,
+        ease: 'elastic.out',
+      });
     },
     { scope: container }
   );
 
   return (
-    <div ref={container} className='container'>
+    <div
+      ref={container}
+      className='container'
+    >
       <div className='heading'>
         <h1>R3F and GSAP Sandbox</h1>
         <p>Check out what I learned so far!</p>
       </div>
       <div className='grid-container'>
-        <GridItem href='developedbyed-followalong' title='Three.js Crash Course for Beginners'>
+        <GridItem
+          href='developedbyed-followalong'
+          title='Three.js Crash Course for Beginners'
+        >
           I did a follow-along with this{' '}
           <a
             href='https://www.youtube.com/watch?v=_OwJV2xL8M8&list=PLVOXfoQ_x6wcFJ8b122nEVC0jnDjoqKlk&index=22'
@@ -39,31 +42,28 @@ const Home = () => {
             video
           </a>{' '}
           by{' '}
-          <a href='https://www.youtube.com/@developedbyed' target='_blank'>
+          <a
+            href='https://www.youtube.com/@developedbyed'
+            target='_blank'
+          >
             developedbyed
           </a>{' '}
-          but I implemented it in React Three Fiber. The tutorial is originally done in vanilla
-          Three.js.
+          but I implemented it in React Three Fiber. The tutorial is originally done in vanilla Three.js.
         </GridItem>
 
-        <GridItem href='mouse-follow' title='Mouse Follow'>
-          I saw this cool effect on a R3F or GSAP example (can't remember) and I wanted to try and
-          replicate it.
+        <GridItem
+          href='mouse-follow'
+          title='Mouse Follow'
+        >
+          I saw this cool effect on a R3F example and I wanted to try and replicate it. I also experimented with
+          Three.js environment and texture maps.
         </GridItem>
       </div>
     </div>
   );
 };
 
-const GridItem = ({
-  href,
-  title,
-  children,
-}: {
-  href: string;
-  title: string;
-  children?: React.ReactNode;
-}) => {
+const GridItem = ({ href, title, children }: { href: string; title: string; children?: React.ReactNode }) => {
   const gridItemRef = React.useRef<HTMLDivElement>(null!);
 
   useGSAP(() => {
@@ -86,7 +86,10 @@ const GridItem = ({
   });
 
   return (
-    <div ref={gridItemRef} className='grid-item'>
+    <div
+      ref={gridItemRef}
+      className='grid-item'
+    >
       <div className='hover-banner' />
       <h2>
         <a href={href}>{title}</a>
--- src/routes/mouseFollow/index.tsx
+++ src/routes/mouseFollow/index.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import { useEffect, useRef } from 'react';
+import { TextureLoader, Color, type Mesh } from 'three';
+import { Canvas, useLoader } from '@react-three/fiber';
+import { Environment } from '@react-three/drei';
+
+import './styles.css';
 
 const MouseFollow = () => {
-  return <div>MouseFollow</div>;
+  return (
+    <div className='container'>
+      <a href='/'>Home</a>
+      <div id='canvas-container'>
+        <Canvas
+          camera={{ position: [0, 0, 50] }}
+          scene={{ background: new Color(0xf0f0f0) }}
+        >
+          <Environment files={'environments/qwantani_afternoon_puresky_2k.hdr'} />
+          <Box />
+          <ambientLight intensity={5} />
+        </Canvas>
+      </div>
+    </div>
+  );
+};
+
+const Box = () => {
+  const meshRef = useRef<Mesh>(null!);
+  const [colorMap, metalnessMap, normalMap, roughnessMap] = useLoader(TextureLoader, [
+    'Metal061B_1K-JPG/Metal061B_1K-JPG_Color.jpg',
+    'Metal061B_1K-JPG/Metal061B_1K-JPG_Metalness.jpg',
+    'Metal061B_1K-JPG/Metal061B_1K-JPG_NormalGL.jpg',
+    'Metal061B_1K-JPG/Metal061B_1K-JPG_Roughness.jpg',
+  ]);
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      meshRef.current.lookAt(
+        (event.clientX / window.innerWidth) * 2 - 1,
+        -(event.clientY / window.innerHeight) * 2 + 1,
+        0.5
+      );
+    };
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[10, 10, 10]} />
+      <meshStandardMaterial
+        color='rgb(4,158,244)'
+        roughness={1}
+        metalness={0.5}
+        map={colorMap}
+        metalnessMap={metalnessMap}
+        normalMap={normalMap}
+        roughnessMap={roughnessMap}
+      />
+    </mesh>
+  );
 };
 
 export default MouseFollow;
