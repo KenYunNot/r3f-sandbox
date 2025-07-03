750ef3d139d5e6bc76500cb0ea6c14a43d2e4ac7
Ken Yun <yunhaen@gmail.com>
2025-07-02 22:29:46 -0400

    feat: added route for buffer geometry demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
 import Home from './routes/(home)/index.tsx';
 import DevelopedbyedFollowalong from './routes/developedbyedFollowalong/index.tsx';
+import MouseFollow from './routes/mouseFollow/index.tsx';
+import BufferGeometryDemo from './routes/bufferGeometryDemo/index.tsx';
 
 import './App.css';
-import MouseFollow from './routes/mouseFollow/index.tsx';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
+        <Route
+          index
+          element={<Home />}
+        />
 
-        <Route path='developedbyed-followalong' element={<DevelopedbyedFollowalong />} />
+        <Route
+          path='developedbyed-followalong'
+          element={<DevelopedbyedFollowalong />}
+        />
 
-        <Route path='mouse-follow' element={<MouseFollow />} />
+        <Route
+          path='mouse-follow'
+          element={<MouseFollow />}
+        />
+
+        <Route
+          path='buffer-geometry-demo'
+          element={<BufferGeometryDemo />}
+        />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/routes/bufferGeometryDemo/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BufferGeometryDemo = () => {
+  return <div>BufferGeometryDemo</div>;
+};
+
+export default BufferGeometryDemo;
